d88d49ddaec209644ea10f591d6560764adeec93
danieltorrez29 <daniel.torres04@uptc.edu.co>
2024-05-10 20:06:44 -0500

    Initial version

---
+++ src/pages/LogInPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import LogIn from "mf_login/LogIn";
+
+const LogInPage = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        background: "linear-gradient(to left, #ffffff 50%, #23294b 50%)",
+        padding: 0,
+      }}
+    >
+      <LogIn />;
+    </div>
+  );
+};
+
+export default LogInPage;
