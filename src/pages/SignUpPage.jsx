4fee66d63b93c20fa1235bc728dac5188fa8d1c7
Catalina Fajardo <tania.fajardo01@uptc.edu.co>
2024-05-20 12:27:44 -0500

    style sepadaro singup

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -4,13 +4,7 @@ import SignUp from "mf_signup/SignUp";
 
 const SignUpPage = () => {
   return (
-    <div
-      style={{
-        height: "100vh",
-        background: "linear-gradient(to left, #ffffff 50%, #23294b 50%)",
-        padding: 0,
-      }}
-    >
+    <div>
       <SignUp />
     </div>
   );
